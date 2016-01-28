6f2f970e1c651f517306bc7fdf6d2e6e62cec2b6
jaredSalazar <jared@techaguru.com>
2016-01-29 03:06:14 +0800

    add blinking thread and system restart ever 1 hour

--- Weather_Sensor_Device_Ubidots.ino
+++ Weather_Sensor_Device_Ubidots.ino
@@ -5,45 +5,84 @@
 #include <easyLStorage.h>
 
 GPRSL global;
-int LastSendTime = 0;
+int LastSendTime = 0,nowSending = 0, reboot_count = 0;
+const int send_interval = 60000;
 
 void setup() {
   Serial.begin(9600);
   blinkSetup();
-  
+
+  LTask.begin();
+
   Serial.println("RTC Sync...");
   rtcSync();
 
   global.START();
-  
+
   StorageStart();
-  
+  Log("Device Starting....");
+
   rainSensorAttach();
-  
+
   blinker(3000);
 
+  LTask.remoteCall(createThread, NULL);
   delay(15000);
 }
 
 void loop() {
-  if ((millis()) - LastSendTime > 15000) {
+  if ((millis()) - LastSendTime > send_interval) {
+    Log("Sending now...");
+    nowSending = 1;
     LastSendTime = millis();
     String var = jsonContruct();
     String response = gprsSend(var);
     Serial.println(response);
     checkResponse(var, response);
     blinker(100);
+    delay(100);
   }
-  if(!checkSD()){
-    int delayTime = 15000 - millis() + LastSendTime;
-    if(delayTime < 0){
+  if (!checkSD()) {
+    int delayTime = send_interval - millis() + LastSendTime;
+    if (delayTime < 0) {
       delayTime = 0;
     }
     Serial.print("delay Time: ");
     Serial.println(delayTime);
+    nowSending = 0;
     delay(delayTime);
-  }else{
-    delay(200);
+  } else {
+    delay(10);
   }
+}
+
+boolean createThread(void* userdata) {
+  vm_thread_create(blinker_thread, NULL, 0);
+  return true;
+}
+
+boolean call_reboot(void* userdata) {
+  vm_reboot_normal_start();
+  return true;
+}
+
+VMINT32 blinker_thread(VM_THREAD_HANDLE thread_handle, void* user_data) {
+  for (;;)
+  {
+    if(nowSending == 0){
+      Serial.printf("\nblinker thread... still running %d \n", reboot_count);
+    if (reboot_count == 360) {
+      Serial.printf("\nReboot...\n");
+      Log("Device reboot...");
+      LTask.remoteCall(call_reboot, NULL);
+    }
+    blinker(100);
+    reboot_count++;
+    delay(10000);
+    }else{
+      delay(100);
+    }
+  }
+  return 0;
 }
 
